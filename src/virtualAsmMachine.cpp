bf2e8c0d2c5ce03143df78d33a14760fbdf4d278
Omid N <onajafi141@gmail.com>
2024-07-16 20:45:41 +0330

    Implement a simple virtual test on the computer

--- src/virtualAsmMachine.cpp
+++ src/virtualAsmMachine.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
+#include <memory>
+#include <chrono>
+#include <thread>
 #include "./cpu/cpu8008.h"
 #include "./peripheral/virtualInstructionMemory.h"
 
-Cpu8008* cpu8008;
-InstructionMemory* instMem;
-InstructionMemory* virtualInstMem;
+// Cpu8008* cpu8008;
+// InstructionMemory* instMem;
+// InstructionMemory* virtualInstMem;
 
-// Function to test part 1 of the project
-void testPart1() {
-    // Your code here
-    std::cout << "Testing part 1..." << std::endl;
-}
 
-// Function to test part 2 of the project
-void testPart2() {
-    // Your code here
-    std::cout << "Testing part 2..." << std::endl;
-}
+
+
+#include <bitset>
 
 int main() {
     // Call the test functions
-    testPart1();
-    testPart2();
+    std::shared_ptr<Cpu8008> cpu8008 = std::make_shared<Cpu8008>();
+    std::shared_ptr<VirtualInstructionMemory> virtualInstMem = std::make_shared<VirtualInstructionMemory>();
+
+    //Simulate the instruction memory
+    virtualInstMem->setInstruction(0x00, 0b01000100);
+    virtualInstMem->setInstruction(0x01, 0x04);
+    virtualInstMem->setInstruction(0x02, 0b01000100);
+    virtualInstMem->setInstruction(0x03, 0x00);
+    virtualInstMem->setInstruction(0x04, 0b01000100);
+    virtualInstMem->setInstruction(0x05, 0x02);
+
+    //Simulate the cpu8008
+    int pc = 0;
+    for(int i=0; i<50; i++){
+        byte inst;
+        inst = virtualInstMem->getInstruction(pc);
+        std::bitset<8> binary(inst);
+        std::cout << "[" << pc << "]:\t" << binary << std::endl;
+        
+        pc = cpu8008->processInstruction(inst);
+        // std::cout << "Next PC:\t" << pc << std::endl;
+
+        //wait for 0.5 seconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        
+    }
 
     return 0;
 }
